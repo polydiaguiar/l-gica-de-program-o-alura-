e8ae39f92a95070388f02d3e16b9ef72d63c2888
unknown <polyana.karynne@hotmail.com>
2024-01-19 22:56:55 -0300

    test: cria contador progressivo e regressivo

---
+++ index.js
@@ -0,0 +1,28 @@
+// Desafios
+
+
+
+
+//Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, 
+//usando um loop while no console do navegador.
+
+//Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, 
+//usando um loop while no console do navegador.
+
+// Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+let contador = 0;
+
+while (contador<10){
+    contador ++
+    console.log(contador);
+
+}
+
+// Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+ contador = 10;
+
+while (contador>0){
+    contador --
+    console.log(contador);
+
+}
